13e7df17ba2069f6dff8ae90aa574b8b4eb10c24
Artur Symanovic <artur.symanovic@gmail.com>
2020-07-06 14:05:18 +0100

    Fixed bug in Health component

--- Source/ToonTanks/Components/HealthComponent.cpp
+++ Source/ToonTanks/Components/HealthComponent.cpp
@@ -10,8 +10,6 @@ UHealthComponent::UHealthComponent()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = false;
-
-	Health = DefaultHealth;
 }
 
 
@@ -19,6 +17,7 @@ UHealthComponent::UHealthComponent()
 void UHealthComponent::BeginPlay()
 {
 	Super::BeginPlay();
+	Health = DefaultHealth;
 
 	GameModeRef = Cast<ATankGameModeBase>(UGameplayStatics::GetGameMode(GetWorld()));
 

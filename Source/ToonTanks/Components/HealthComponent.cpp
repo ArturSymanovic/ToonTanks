25c8345a2ab33115f8644f6634652bd370f8bd17
Artur Symanovic <artur.symanovic@gmail.com>
2020-07-06 11:04:16 +0100

    Created Health component class

---
+++ Source/ToonTanks/Components/HealthComponent.cpp
@@ -0,0 +1,35 @@
+// Copyright Artur Symanovic 2020
+
+
+#include "HealthComponent.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+void UHealthComponent::TakeDamage(
+	AActor* DamageActor, 
+	float Damage, 
+	const UDamageType* DamageType, 
+	AController* InstigatedBy, 
+	AActor* DamageCauser
+)
+{
+
+}
---
+++ Source/ToonTanks/Components/HealthComponent.h
@@ -0,0 +1,41 @@
+// Copyright Artur Symanovic 2020
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+class ATankGameModeBase;
+class UDamageType;
+class AController;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TOONTANKS_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UHealthComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	void TakeDamage(
+		AActor* DamageActor, 
+		float Damage, 
+		const UDamageType* DamageType, 
+		AController* InstigatedBy, 
+		AActor* DamageCauser
+	);
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Health", meta = (AllowPrivateAccess = "true"))
+	float DefaultHealth = 100.0f;
+	float Health = 0.0f;
+	
+
+	AActor* Owner;
+	ATankGameModeBase* GameModeRef;
+};
